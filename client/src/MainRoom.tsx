f70a6427c7769bc3a072cc5d69872e8d1ad593dc
Stormi <stearnsessie@gmail.com>
2025-12-09 17:19:02 -0500

    n

--- client/src/MainRoom.tsx
+++ client/src/MainRoom.tsx
@@ -3,10 +3,24 @@ import { useState } from 'react'
 import './App.css'
 
 function MainRoom() {
-const [ form, setForm ] = useState({
+
+const [ message , setMessage ] = useState({
     username: " ",
     message: " ",
-})
+});
+
+if (!message.trim()) return;
+
+//message template
+const newMessage = {
+    text: message.message,
+    username: message.username,
+    createdAt: new Date().toLocaleTimeString(),// new is a invocation alert to let you know that we are using date as a template
+};
+
+setMessage( (prev:string) => [...prev, newMessage]);
+setMessage("");
+
 }
 
-export default App
+export default MainRoom;

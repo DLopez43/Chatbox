a34a0f56380cf34babfc025792c196804d3ee627
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-12-09 14:08:48 -0500

    File Setup

---
+++ client/src/MainRoom.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function MainRoom() {
+const [ form, setForm ] = useState({
+    username: " ",
+    message: " ",
+})
+}
+
+export default App
